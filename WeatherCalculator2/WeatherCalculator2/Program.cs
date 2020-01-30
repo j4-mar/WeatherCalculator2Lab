2add601bdbe0b9227bc6b289efcbb738a650ea47
j4-mar <59981843+j4-mar@users.noreply.github.com>
2020-01-30 16:38:32 -0600

    Added weather calculations

--- WeatherCalculator2/WeatherCalculator2/Program.cs
+++ WeatherCalculator2/WeatherCalculator2/Program.cs
@@ -20,18 +20,43 @@ namespace WeatherCalculator2
 
             temperature = int.Parse(input);
 
-            // Get the windspeed from the user
-            Console.WriteLine("Enter the wind speed");
+            //// Get the windspeed from the user
+            //Console.WriteLine("Enter the wind speed");
 
-            input = Console.ReadLine();
+            //input = Console.ReadLine();
 
-            windSpeed = int.Parse(input);
+            //windSpeed = int.Parse(input);
 
-            // Calculate the wind chill
+            //// Calculate the wind chill
 
-            windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+            //windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
 
-            Console.WriteLine("The wind chill is " + windChill);
+            //Console.WriteLine("The wind chill is " + windChill);
+
+            if (temperature > 50)
+            {
+                int RH;
+
+                Console.WriteLine("Enter the relative humidity");
+                input = Console.ReadLine();
+                RH = int.Parse(input);
+
+                double HI = 0.5 * (temperature + 61) + (temperature - 68) * 1.2 + (RH * 0.094);
+                
+
+                Console.WriteLine("The Heat Index is" + HI);
+            }
+            else
+            {
+                Console.WriteLine("Enter the wind speed");
+                input = Console.ReadLine();
+                windSpeed = int.Parse(input);
+
+                windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+
+                Console.WriteLine("The wind chill is" + windChill);
+            }
+
         }
     }
 }
